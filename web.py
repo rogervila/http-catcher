a9741c38c0dbe1dc86f20b3d643d610209d367fe
Roger Vilà <rogervila@me.com>
2021-06-29 12:02:07 +0200

    Create web.py

---
+++ web.py
@@ -0,0 +1,48 @@
+from flask import Flask, request, jsonify
+from json import dumps
+from logging.config import dictConfig
+
+# https://stackoverflow.com/a/56998012/3333549
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://sys.stdout',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'DEBUG',
+        'handlers': ['wsgi']
+    }
+})
+
+HTTP_METHODS = ['GET', 'POST', 'PUT', 'PATCH',
+                'DELETE', 'COPY', 'HEAD', 'OPTIONS',
+                'LINK', 'UNLINK', 'PURGE', 'LOCK',
+                'UNLOCK', 'PROPFIND', 'VIEW', 'CONNECT', 'TRACE']
+
+app = Flask(__name__)
+
+
+@app.route('/', defaults={'path': ''}, methods=HTTP_METHODS)
+@app.route('/<path:path>', methods=HTTP_METHODS)
+def catch(path):
+    data = {
+        'headers': dict(request.headers),
+        'data': request.data.decode('utf-8'),
+        'args': request.args,
+        'form': request.form,
+        'endpoint': request.endpoint,
+        'method': request.method,
+        'remote_addr': request.remote_addr,
+        'path': path,
+    }
+
+    # pylint: disable=no-member
+    app.logger.debug(dumps(data, indent=2, sort_keys=True))
+    # pylint: enable=no-member
+
+    return jsonify(data)
